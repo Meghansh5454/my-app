1c4ad02c41feea64c0f5f13ec9c97111078361ad
Meghansh5454 <meghansh4455@gmail.com>
2025-01-01 09:42:44 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div className='container' ><TextForm heading="Enter the text to Analyze Below" /></div>
+    
+    </>
+  );  
+}
+
+export default App;
